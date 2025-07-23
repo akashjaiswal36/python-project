b831501c3657d7b86f5855cfdbf878aad6538305
akashjaiswal36 <68245389+akashjaiswal36@users.noreply.github.com>
2025-07-23 21:23:58 +0000

    push all

--- jira-api-call-github-python/jira_ticket.py
+++ jira-api-call-github-python/jira_ticket.py
@@ -18,7 +18,7 @@ def createJIRA():
 
     url = "https://akashjaiswal-1746203677846.atlassian.net/rest/api/3/issue"
 
-    API_TOKEN = "ATATT3xFfGF0XAxuNms0D4n8HzQH8Iw7hFm0YWgF7tLO4agczBlI3MOWZK8iGDO4Yd6GcpwUm6yVPtkyVLqkgGRUaUrCtODooV05zRuD1ihZrgWGV49Dtb62UlecWf3yhfa3Q_neF7RSnpl2zZZT65NOKUNsUMEDL732M7Zj-bpOAITfVJwHors=4A2696BC"
+    API_TOKEN = " "
 
     auth = HTTPBasicAuth("akashjaiswal@live.in", API_TOKEN)
 
