0b396f2055bd6edc0841663faa13a108989e0817
akashjaiswal36 <68245389+akashjaiswal36@users.noreply.github.com>
2025-07-23 21:21:42 +0000

    push all

---
+++ jira-api-call-github-python/jira_ticket.py
@@ -0,0 +1,58 @@
+from flask import Flask, request
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+# creating flask app instance
+app = Flask(__name__)
+
+@app.route("/createJIRA", methods=['POST'])
+def createJIRA():
+
+    # GitHub sends JSON payload — parse it
+    data = request.json
+
+    # Extract info from GitHub issue (adjust keys as needed)
+    github_title = data.get("issue", {}).get("title", "No Title Provided")
+    github_body = data.get("comment", {}).get("body", "No Description Provided")
+
+    url = "https://akashjaiswal-1746203677846.atlassian.net/rest/api/3/issue"
+
+    API_TOKEN = "ATATT3xFfGF0XAxuNms0D4n8HzQH8Iw7hFm0YWgF7tLO4agczBlI3MOWZK8iGDO4Yd6GcpwUm6yVPtkyVLqkgGRUaUrCtODooV05zRuD1ihZrgWGV49Dtb62UlecWf3yhfa3Q_neF7RSnpl2zZZT65NOKUNsUMEDL732M7Zj-bpOAITfVJwHors=4A2696BC"
+
+    auth = HTTPBasicAuth("akashjaiswal@live.in", API_TOKEN)
+
+    headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+    "fields": {
+        "issuetype": {
+        "id": "10036"
+        },
+        "project": {
+        "key": "AK"
+        },
+        "summary": github_title,
+    },
+    "update": {}
+    } )
+
+    if github_body == "/jira":
+        response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+        )
+
+        print("githun_body:", github_body)
+        return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+    else:
+        print("GitHub Body not '/jira'. Skipping JIRA creation.")
+        return json.dumps({"message": "JIRA not created. Condition not met."}, indent=4)
+
+app.run('0.0.0.0')
